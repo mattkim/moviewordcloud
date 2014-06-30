4e0c16f90d55350f7f86f6ea6141db98ad19db4e
Matthew Kim <mattkim@Matthews-MacBook-Pro.local>
2014-06-29 20:14:12 -0700

    Changing really lame pw

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "mattkim", password: "fmsf23", except: [:index, :show]
 
   def new
     @article = Article.new
