bd1e20099ce512d006ce0d2fa5f713e08b2d0296
Matthew Kim <mattkim@Matthews-MacBook-Pro.local>
2015-02-28 00:32:20 -0800

    Trying to get images on top row

--- app/assets/javascripts/wordcloud.js
+++ app/assets/javascripts/wordcloud.js
@@ -55,7 +55,7 @@ function displayMovies() {
     Object.keys(movies).forEach(function (key) { 
       var m = movies[key];
       var image = $("<div/>", {
-        class: "small-2 medium-2 large-2 columns left text-left wcimg"
+        class: "columns"
       }).append("<img src='"+m["poster_detailed"]+"'></img>");
 
       var title = $("<div/>", {
