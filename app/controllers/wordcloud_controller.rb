1e50ff143e4b508166576e21a09f9ce540e3b6b7
mattkim <matthkim@gmail.com>
2015-05-22 21:46:42 -0700

    Pulling only medium sized files now for movie poster images

--- app/controllers/wordcloud_controller.rb
+++ app/controllers/wordcloud_controller.rb
@@ -29,7 +29,7 @@ class WordcloudController < ApplicationController
   # legacy google image search api and current google custom search
   def getImgURL(title, year)
     title = URI.escape(title + " " + year.to_s + " movie poster")
-    req = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAyLoJQc-3aOYZLlHff3S4JPmeK88rL878&cx=015799936154194163641:2d_yj8n3fbm&q="+title+"&searchType=image"
+    req = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAyLoJQc-3aOYZLlHff3S4JPmeK88rL878&cx=015799936154194163641:2d_yj8n3fbm&q="+title+"&searchType=image&imgSize=medium"
     img_results = JSON.parse(call_ssl(req))
     if img_results && img_results["items"]
       img_results["items"].each do |img_result|
